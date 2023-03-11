d177ba472cbd1b52c5bf0b8ce5466413d241479c
gregnani <fernandou.gregnani@gmail.com>
2023-03-10 22:38:01 -0300

    exercicio concluido

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("Mustang Preto");
+Console.WriteLine("Joga esse Mustang preto na minha garagem");
+Console.WriteLine("Ou uma Benz, ou uma Lambo', tanto faz, yeah, yeah");
+Console.WriteLine("No futuro, eu me vi comprando carros que me convenham");
+Console.WriteLine("Porsche, Rolls-Royce, que é pra ela poder mamar em paz");
+Console.WriteLine("Joga esse Mustang preto na minha garagem, yeah");
+Console.WriteLine("Ou uma Benz, ou uma Lambo', tanto faz, yeah, yeah");
+Console.WriteLine("No futuro, eu me vi comprando carros que me convenham, yeah");
+Console.WriteLine("Fazendo tudo que um dia eu queria");
+Console.WriteLine("Vejo lembranças em fotografias, yeah");
+Console.ResetColor();
+Console.WriteLine("Eu já fiz muito por essa vida, nego, então não pare");
+Console.WriteLine("Jogo todo esse dígito no pescoço da minha Barbie");
+Console.WriteLine("Backwoods pela manhã, fumando dia e noite, nunca pare");
+Console.WriteLine("Essa vadia quer fuder comigo e me jogar atrás dessa grade");
+Console.WriteLine("Eu nasci, eu cresci, aprendi a chorar e sorrir");
+Console.WriteLine("Sempre que sorri, sou rei (rei), dando block nesses fake'");
+Console.WriteLine("Todo dinheiro que eu tinha, eu dividi");
+Console.WriteLine("Bolos de notas de dez e de vinte");
+Console.WriteLine("Trinta K na minha jaqueta de grife");
+Console.WriteLine("Todo o meu time usando Givenchy");
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("Joga esse Mustang preto na minha garagem, yeah");
+Console.WriteLine("Ou uma Benz, ou uma Lambo' (slime), tanto faz, yeah, yeah");
+Console.WriteLine("No futuro, eu me vi comprando carros que me convenham, yeah");
+Console.WriteLine("Porsche ou Rolls-Royce, que é pra ela poder mamar em paz");
+Console.ResetColor();
+Console.WriteLine("Sabe que não me engana");
+Console.WriteLine("E, mesmo assim, tu fala que não é minha fã");
+Console.WriteLine("Todo o final de semana tu queria 'tá dentro da minha van");
+Console.WriteLine("Rolê de Porsche Macan, só quer se for o celular da maçã");
+Console.WriteLine("Só diz que é minha em cima da cama");
+Console.WriteLine("Eu digo pra ela que ela não me engana");
+Console.WriteLine("Sou muito mais do que só mais um na cena");
+Console.WriteLine("A concorrência não aguenta, ih, ih, ih, ih");
+Console.WriteLine("Se tu entrar na minha van, tu só vai sair depois de amanhã");
+Console.WriteLine("Fumando becks da pura skunk");
+Console.WriteLine("A maldade 'tá em quem mordeu a maçã");
+Console.WriteLine("Ih, ih, ih");
+Console.WriteLine("No final, eu vou ser mais que esses pelas");
+Console.WriteLine("Vão ter que me aturar, yeah");
+Console.ForegroundColor = ConsoleColor.Cyan
+Console.WriteLine("Joga esse Mustang preto na minha garagem, yeah");
+Console.WriteLine("Ou uma Benz, ou uma Lambo, tanto faz, yeah");
+Console.WriteLine("No futuro, eu me vi comprando carros que me convenham, yeah");
+Console.WriteLine("Porsche ou Rolls-Royce, que é pra ela poder mamar em paz;");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
